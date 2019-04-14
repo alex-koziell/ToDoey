8ac5d20b43d7e0f8e5d8897b6ca8f829fa965395
Alex K <alexUsesCamelCase@katrinas-mbp.home>
2019-04-14 14:34:04 +0100

    Convert to MVC design pattern.

---
+++ ToDoey/Data Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  ToDoey
+//
+//  Created by Alex K on 14/04/2019.
+//  Copyright © 2019 Alex K. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title : String = ""
+    var done : Bool = false
+    
+}
